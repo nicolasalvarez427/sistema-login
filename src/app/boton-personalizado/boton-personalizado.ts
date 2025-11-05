a4ba428d7d3fe243bc09b97488e25a7a011b723d
nicolasalvarez427 <nicolasalvarez427@gmail.com>
2025-11-05 14:12:25 -0300

    base login

---
+++ src/app/boton-personalizado/boton-personalizado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-personalizado',
+  imports: [],
+  templateUrl: './boton-personalizado.html',
+  styleUrl: './boton-personalizado.css',
+})
+export class BotonPersonalizado {
+
+}
---
+++ src/app/panel-admin/panel-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-admin',
+  imports: [],
+  templateUrl: './panel-admin.html',
+  styleUrl: './panel-admin.css',
+})
+export class PanelAdmin {
+
+}
